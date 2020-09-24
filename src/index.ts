0c6c900cfa08dd848390064e06c2f06ee6b035db
Sergio Moura <sergio@moura.ca>
2020-09-24 13:27:03 -0400

    fix index.ts to use danger globals

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import * as danger from 'danger';
 import { existsSync, readFileSync } from 'fs';
+
+declare function fail(message?: string): void;
+declare function markdown(message?: string): void;
 
 // typescript check
 function tscCheck(filename: string = 'tsc_raw.log') {
   if (existsSync(filename)) {
     const log = readFileSync(filename, 'utf8');
     if (log.length) {
-      danger.fail('TypeScript hasn\'t passed, see below for full logs');
-      danger.markdown(`### TypeScript Fails\n\n\`\`\`${log}\`\`\``);
+      fail('TypeScript hasn\'t passed, see below for full logs');
+      markdown(`### TypeScript Fails\n\n\`\`\`${log}\`\`\``);
     }
   }
 }
